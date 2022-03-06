a3c1288223dfb9cd2ad3a7ead2d175b7b7de706b
Ayamie <ayamie.kundando@contech.university>
2022-03-06 17:09:42 +0200

    scrapy web scraping project with data

---
+++ songs/songs/spiders/songsSpyder.py
@@ -0,0 +1,14 @@
+import scrapy
+# from beforward.items import BeforwardItem
+# from scrapy.loader import ItemLoader
+
+class SongsItem(scrapy.Spider):
+    name = 'songs'
+    start_urls = ['https://notjustok.com/category/mp3-songs/']
+
+    def parse(self, response):
+        for car in response.css('li.main-card-list'):
+            yield{
+                'name': car.css('h3::text').extract_first(),
+                'type': car.css('a.card-meta-category::text').get()
+            }
